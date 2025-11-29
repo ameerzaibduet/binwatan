771ea5be6f9f61f3aaaf72abf10169c298858500
Ameer Zaib <ameerzaibduet@gmail.com>
2025-11-29 18:39:48 +0500

    Ticktok pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+
         {/* ✅ Google Analytics */}
         <Script
           async
@@ -63,6 +64,58 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
           />
         </noscript>
+
+        {/* ✅ TikTok Pixel */}
+        <Script
+          id="tiktok-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function (w, d, t) {
+                w.TiktokAnalyticsObject = t;
+                var ttq = w[t] = w[t] || [];
+                ttq.methods = [
+                  "page","track","identify","instances","debug","on","off",
+                  "once","ready","alias","group","enableCookie","disableCookie",
+                  "holdConsent","revokeConsent","grantConsent"
+                ];
+                ttq.setAndDefer = function(t, e) {
+                  t[e] = function() {
+                    t.push([e].concat(Array.prototype.slice.call(arguments, 0)))
+                  }
+                };
+                for (var i = 0; i < ttq.methods.length; i++) {
+                  ttq.setAndDefer(ttq, ttq.methods[i]);
+                }
+                ttq.instance = function(t) {
+                  for (var e = ttq._i[t] || [], n = 0; n < ttq.methods.length; n++)
+                    ttq.setAndDefer(e, ttq.methods[n]);
+                  return e;
+                };
+                ttq.load = function(e, n) {
+                  var r = "https://analytics.tiktok.com/i18n/pixel/events.js";
+                  ttq._i = ttq._i || {};
+                  ttq._i[e] = [];
+                  ttq._i[e]._u = r;
+                  ttq._t = ttq._t || {};
+                  ttq._t[e] = +new Date;
+                  ttq._o = ttq._o || {};
+                  ttq._o[e] = n || {};
+                  var s = d.createElement("script");
+                  s.type = "text/javascript";
+                  s.async = !0;
+                  s.src = r + "?sdkid=D4LFDTRC77U10O2JEKV0&lib=" + t;
+                  var a = d.getElementsByTagName("script")[0];
+                  a.parentNode.insertBefore(s, a);
+                };
+
+                ttq.load('D4LFDTRC77U10O2JEKV0');
+                ttq.page();
+              }(window, document, 'ttq');
+            `,
+          }}
+        />
+
       </head>
 
       <body className="flex flex-col min-h-screen">
